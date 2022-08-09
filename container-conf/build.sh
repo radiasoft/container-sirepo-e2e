58a6be62a6c2b5925f5a193e56f9581039dae971
garsuga <gsugarbaker@gmail.com>
2022-08-09 21:51:11 +0000

    remove build_run_as_user lines

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -17,8 +17,5 @@ build_as_root() {
 }
 
 build_as_run_user() {
-    install_url radiasoft/sirepo
-    #POSIT: This relies on the fact that individual package names don't have spaces or special chars
-    npm install -g \
-        $(install_download package.json | jq -r '.devDependencies | to_entries | map("\(.key)@\(.value)") | .[]')
+    
 }
