51e5f2bddf2b8e66cecba263d375d0e81da699c6
garsuga <gsugarbaker@gmail.com>
2022-08-09 21:47:58 +0000

    wip playwright testing setup

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-build_image_base=radiasoft/fedora
+build_image_base=${sirepo_ci_base:-radiasoft/beamsim}
 build_is_public=1
+
+build_as_root() {
+    umask 022
+    build_yum install nodejs16
+    npm install -g npx
+    npm install -g @playwright/test
+    cd /playwright
+    npx playwright test
+}
+
+build_as_run_user() {
+    install_url radiasoft/sirepo
+    #POSIT: This relies on the fact that individual package names don't have spaces or special chars
+    npm install -g \
+        $(install_download package.json | jq -r '.devDependencies | to_entries | map("\(.key)@\(.value)") | .[]')
+}
