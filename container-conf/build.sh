54d1eebb8fc32c8d32976c6fe960076a56b850b4
garsuga <gsugarbaker@gmail.com>
2022-08-15 17:45:52 +0000

    wip reorganize commands

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -3,28 +3,10 @@ build_image_base=radiasoft/fedora
 build_is_public=1
 
 build_as_root() {
-    umask 022
-    build_yum install nodejs16
-    npm install -g npx
-    npm install -g @playwright/test
-
-    export SIREPO_E2E_USER=test_user
-    export SIREPO_ENDPOINT=sirepo.com
-
-    cd $build_guest_conf/playwright
-    # TODO: replace endpoint in playwright config
-    npx playwright test
-
-
-    # TODOS: garsuga
-    # somehow tell it where to go
-    # login stuff
-
-    
-
-
+    build_yum install nodejs
 }
 
 build_as_run_user() {
-    
+    npm install -g npx @playwright/test
+    # TODO: garsuga setup browsers????
 }
